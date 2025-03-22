10f6bd84233d657a8211825871c7e6006099c2fc
Kittipong Tapyou <kittipong.tpy@gmail.com>
2025-03-22 19:26:33 +0700

    added alert border

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,15 +1,27 @@
 import { Link } from "@heroui/link";
 
-import { Navbar } from "@/components/navbar";
+import { ThemeSwitch } from "@/components/theme-switch";
 
 export default function DefaultLayout({
   children,
+  isRobot,
 }: {
   children: React.ReactNode;
+  isRobot?: "yes" | "no";
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
-      <Navbar />
+    <div
+      className={`relative flex flex-col h-screen ${
+        isRobot === "yes" ? "border-2 border-red-500 animate-pulse" : ""
+      }`}
+      style={{
+        boxShadow: isRobot === "yes" ? "inset 0 0 30px 10px red" : undefined,
+      }}
+    >
+      <div className="absolute top-4 right-4">
+        <ThemeSwitch />
+      </div>
+      {/* <Navbar /> */}
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,27 +1,69 @@
 import React from "react";
-import { Form, Input, Button } from "@heroui/react";
+import {
+  Form,
+  InputOtp,
+  Button,
+  NumberInput,
+  Select,
+  SelectItem,
+  DatePicker,
+  Switch,
+} from "@heroui/react";
 
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
-
+export const animals = [
+  { key: "cat", label: "Cat" },
+  { key: "dog", label: "Dog" },
+  { key: "elephant", label: "Elephant" },
+  { key: "lion", label: "Lion" },
+  { key: "tiger", label: "Tiger" },
+  { key: "giraffe", label: "Giraffe" },
+  { key: "dolphin", label: "Dolphin" },
+  { key: "penguin", label: "Penguin" },
+  { key: "zebra", label: "Zebra" },
+  { key: "shark", label: "Shark" },
+  { key: "whale", label: "Whale" },
+  { key: "otter", label: "Otter" },
+  { key: "crocodile", label: "Crocodile" },
+];
 export default function App() {
   const [submitted, setSubmitted] = React.useState(null);
+  const [status, setStatus] = React.useState<"yes" | "no">("no");
 
-  const onSubmit = (e) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.currentTarget));
 
-    setSubmitted(data);
+    // setSubmitted(data);
+    setStatus("yes");
   };
 
   return (
-    <DefaultLayout>
+    <DefaultLayout isRobot={status}>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Enter the form</h1>
+          <span
+            className={title({
+              class:
+                "bg-gradient-to-r from-[#603813] to-[#b29f94] bg-clip-text text-transparent",
+            })}
+          >
+            CAPTUCHINO
+          </span>
+          <span className={title({})}>☕</span>
         </div>
         <Form className="w-full max-w-xs" onSubmit={onSubmit}>
+          {/* <Input
+            isRequired
+            errorMessage="Please enter a valid username"
+            label="Username"
+            labelPlacement="outside"
+            name="username"
+            placeholder="Enter your username"
+            type="text"
+          />
           <Input
             isRequired
             errorMessage="Please enter a valid email"
@@ -30,7 +72,38 @@ export default function App() {
             name="email"
             placeholder="Enter your email"
             type="email"
+          /> */}
+          <DatePicker label="Date" labelPlacement="outside" />
+          <NumberInput
+            className="max-w-xs"
+            label="Number"
+            labelPlacement="outside"
+            placeholder="Enter the amount"
           />
+          <InputOtp
+            isRequired
+            aria-label="OTP input field"
+            label="OTP"
+            length={7}
+            name="otp"
+            placeholder="Enter code"
+          />
+          <Select
+            className="max-w-xs"
+            label="Favorite Animal"
+            labelPlacement="outside"
+            placeholder="Select an animal"
+          >
+            {animals.map((animal) => (
+              <SelectItem key={animal.key}>{animal.label}</SelectItem>
+            ))}
+          </Select>
+          <Switch defaultSelected size="sm">
+            Enable A function
+          </Switch>
+          <Switch defaultSelected size="sm">
+            Enable B function
+          </Switch>
           <Button type="submit" variant="bordered">
             Submit
           </Button>
