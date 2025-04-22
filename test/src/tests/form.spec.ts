9ded12d51b937158646629fc67b16013aad5fd31
Kittipong Tapyou <kittipong.tpy@gmail.com>
2025-04-22 17:59:01 +0700

    added url checker

--- test/src/tests/form.spec.ts
+++ test/src/tests/form.spec.ts
@@ -3,33 +3,36 @@ import { LocatorFormPage } from '../pages/locatorform.spec';
 import { TabFormPage } from '../pages/tabform.spec';
 import testCase from '../test-data/testCase';
 
-test.describe("Input by locator", () => {
-    testCase.forEach((testData) => {
-        test.only(`locator typing : ${testData.fullName} `, async ({ page }) => {
-            const formPage = new LocatorFormPage(page);
 
-            await formPage.goto();
-            await formPage.fillFullName(testData.fullName, true);
+// test.describe("Input by locator", () => {
+//     testCase.forEach((testData) => {
+//         test.only(`locator typing : ${testData.fullName} `, async ({ page }) => {
+//             const formPage = new LocatorFormPage(page);
 
-            //wait for 2 sec
-            await formPage.fillNumber(testData.number, true);
-        }
-        )
-    })
+//             await formPage.goto();
+//             await formPage.fillFullName(testData.fullName, true);
 
-    testCase.forEach((testData) => {
-        test.only(`locator paste : ${testData.fullName} `, async ({ page }) => {
-            const formPage = new LocatorFormPage(page);
+//             //wait for 2 sec
+//             await formPage.fillNumber(testData.number, true);
+//         }
+//         )
+//     })
 
-            await formPage.goto();
-            await formPage.fillFullName(testData.fullName, false);
+//     testCase.forEach((testData) => {
+//         test.only(`locator paste : ${testData.fullName} `, async ({ page }) => {
+//             const formPage = new LocatorFormPage(page);
 
-            //wait for 2 sec
-            await formPage.fillNumber(testData.number, false);
-        }
-        )
-    })
-})
+//             await formPage.goto();
+//             await formPage.fillFullName(testData.fullName, false);
+
+//             //wait for 2 sec
+//             await formPage.fillNumber(testData.number, false);
+//         }
+//         )
+//     })
+// })
+
+
 test.describe("Input by tab", () => {
     testCase.forEach((testData) => {
         test.only(`tab typing : ${testData.fullName} `, async ({ page }) => {
@@ -39,13 +42,18 @@ test.describe("Input by tab", () => {
             await formPage.firstClick();
             await formPage.fillFullName(testData.fullName, true);
             await page.keyboard.press('Tab');
-            await page.keyboard.press('Tab');
-            await page.keyboard.press('Tab');
+            await formPage.fillMonth(testData.month, true);
+            await formPage.fillDate(testData.date, true);
+            await formPage.fillYear(testData.year, true);
             await page.keyboard.press('Tab');
             await page.keyboard.press('Tab');
 
             //wait for 2 sec
             await formPage.fillNumber(testData.number, true);
+            await page.keyboard.press('Tab');
+            await formPage.fillOtp(testData.otp, true);
+            expect(checkUrl(page.url())).toBe(true);
+
         }
         )
     })
@@ -58,13 +66,20 @@ test.describe("Input by tab", () => {
             await formPage.firstClick();
             await formPage.fillFullName(testData.fullName, false);
             await page.keyboard.press('Tab');
-            await page.keyboard.press('Tab');
-            await page.keyboard.press('Tab');
+            await formPage.fillMonth(testData.month, false);
+            await formPage.fillDate(testData.date, false);
+            await formPage.fillYear(testData.year, false);
             await page.keyboard.press('Tab');
             await page.keyboard.press('Tab');
             //wait for 2 sec
             await formPage.fillNumber(testData.number, false);
+            await page.keyboard.press('Tab');
+            await formPage.fillOtp(testData.otp, false);
+
+            expect(checkUrl(page.url())).toBe(true);
         }
         )
     })
+
+
 })
